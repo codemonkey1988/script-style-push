30142785423e3da9e6305c5c382459ac5fbb6074
Tim Schreiner <schreiner@arndtteunissen.de>
2018-03-27 20:49:22 +0200

    Set version to 1.4.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,5 +18,5 @@ $EM_CONF[$_EXTKEY] = [
     'author'           => 'Tim Schreiner',
     'author_email'     => 'schreiner.tim@gmail.com',
     'author_company'   => '',
-    'version'          => '1.3.0'
+    'version'          => '1.4.0'
 ];
