72aa4c87e2f57e1fb8ebdf2fcf4347ae4bd666f8
Tim Schreiner <schreiner@arndtteunissen.de>
2018-11-05 19:54:04 +0100

    Set minimum TYPO3 version to 9.5.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,7 +6,7 @@ $EM_CONF[$_EXTKEY] = [
     'category'         => 'fe',
     'constraints'       => [
         'depends'   => [
-            'typo3' => '7.6.0-9.5.99'
+            'typo3' => '9.5.0-9.5.99'
         ],
         'conflicts' => [],
         'suggests'  => []
