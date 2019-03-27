11853c328186ff39c1cb0b59453959ac89786886
Tim <schreiner.tim@gmail.com>
2019-03-27 19:44:47 +0100

    [TASK] Set version to 2.1.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,5 +18,5 @@ $EM_CONF[$_EXTKEY] = [
     'author'           => 'Tim Schreiner',
     'author_email'     => 'schreiner.tim@gmail.com',
     'author_company'   => '',
-    'version'          => '2.1.0'
+    'version'          => '2.1.2'
 ];
