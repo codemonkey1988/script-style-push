974bc798d925ddf24e710cf33aab4791a70cdb61
Tim Schreiner <gitlab@tim-schreiner.de>
2020-02-25 23:33:50 +0100

    [TASK] Add TYPO3 v10 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,7 +6,7 @@ $EM_CONF[$_EXTKEY] = [
     'category'         => 'fe',
     'constraints'       => [
         'depends'   => [
-            'typo3' => '9.5.0-9.5.99'
+            'typo3' => '9.5.0-10.3.99'
         ],
         'conflicts' => [],
         'suggests'  => []
